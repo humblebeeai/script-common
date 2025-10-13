354939198acd13100abfd3e62d7f5da49bf05ee2
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-10-13 13:24:26 +0900

    chore: comment out unnecessary directory change in install-basics.sh

--- src/unix/linux/ubuntu/install-basics.sh
+++ src/unix/linux/ubuntu/install-basics.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ## --- Base --- ##
 # Getting path of this script file:
 _SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-"$0"}")" >/dev/null 2>&1 && pwd -P)"
-cd "${_SCRIPT_DIR}" || exit 2
+# cd "${_SCRIPT_DIR}" || exit 2
 
 pwd
 

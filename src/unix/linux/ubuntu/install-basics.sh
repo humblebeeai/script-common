56d83ee9c872b58ce868f7b9c9692a221d3dd527
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-10-13 13:22:24 +0900

    chore: improve script directory path resolution and ensure proper path handling

--- src/unix/linux/ubuntu/install-basics.sh
+++ src/unix/linux/ubuntu/install-basics.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 ## --- Base --- ##
 # Getting path of this script file:
-set +u
-_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-# cd "${_SCRIPT_DIR}" || exit 2
-set -u
+_SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-"$0"}")" >/dev/null 2>&1 && pwd -P)"
+cd "${_SCRIPT_DIR}" || exit 2
+
+pwd
+
+echo "${_SCRIPT_DIR}"
 
 
 # Loading .env file (if exists):

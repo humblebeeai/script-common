23b69eea2d3f8ecde7d3b76c159f66d83812bd15
Batkhuu Byambajav <batkhuu10@gmail.com>
2025-10-13 12:20:43 +0900

    fix: adjust set -u and set +u usage for better script safety

--- src/unix/linux/ubuntu/install-basics.sh
+++ src/unix/linux/ubuntu/install-basics.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 
 ## --- Base --- ##
 # Getting path of this script file:
+set +u
 _SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # cd "${_SCRIPT_DIR}" || exit 2
+set -u
 
 
 # Loading .env file (if exists):
